a40ee60d6af2e263ae2c80075b8a701627053d80
sujalme <sujalj628@gmail.com>
2026-06-14 23:48:38 +0530

    DATA PREPARATION AND MODLE BUILD

--- SUJALJAISWAL_TASK02/Sales.py
+++ SUJALJAISWAL_TASK02/Sales.py
@@ -9,11 +9,23 @@ data = pd.read_csv(r"C:\Users\sujal\OneDrive\Desktop\my_project\OIBSIP\SUJALJAIS
 
 print("++++++ SALES PREDICTION USING PYTHON ++++++\n")
 
-# Dataset Exploration AND FEATURE SELECTION
+# DATASET Exploration AND FEATURE SELECTION
 print("Dataset Shape:", data.shape)
 
 print("\nCOLUMNS IN DATASET:")
 print(data.columns)
 
+# DATA PREPARATION AND SPLITING FOR TARANING
 X = data[['TV', 'Radio', 'Newspaper']]
 y = data['Sales']
+
+
+X_train, X_test, y_train, y_test = train_test_split( X, 
+                                                     y, 
+                                                     test_size=0.2, 
+                                                     random_state=42)
+
+# MODLE
+model = LinearRegression()
+model.fit(X_train, y_train)
+
