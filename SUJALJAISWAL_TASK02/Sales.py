5f551bad2dd256ec0b3c57e09d47d1e6524b73ef
sujalme <sujalj628@gmail.com>
2026-06-14 23:37:09 +0530

    EXPLORING DATASET

--- SUJALJAISWAL_TASK02/Sales.py
+++ SUJALJAISWAL_TASK02/Sales.py
@@ -8,3 +8,12 @@ from sklearn.metrics import r2_score
 data = pd.read_csv(r"C:\Users\sujal\OneDrive\Desktop\my_project\OIBSIP\SUJALJAISWAL_TASK02\Advertising.csv")
 
 print("++++++ SALES PREDICTION USING PYTHON ++++++\n")
+
+# Dataset Exploration AND FEATURE SELECTION
+print("Dataset Shape:", data.shape)
+
+print("\nCOLUMNS IN DATASET:")
+print(data.columns)
+
+X = data[['TV', 'Radio', 'Newspaper']]
+y = data['Sales']
