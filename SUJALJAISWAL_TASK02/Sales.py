60228692e8b256709ca0d2c4e922c39b142ea12f
sujalme <sujalj628@gmail.com>
2026-06-14 23:25:11 +0530

    Import library

---
+++ SUJALJAISWAL_TASK02/Sales.py
@@ -0,0 +1,5 @@
+# Import Libraries
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
