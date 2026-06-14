d832afed1e167efcc83d1c1b352d2af974de961f
sujalme <sujalj628@gmail.com>
2026-06-14 23:29:11 +0530

    loading the data set

--- SUJALJAISWAL_TASK02/Sales.py
+++ SUJALJAISWAL_TASK02/Sales.py
@@ -3,3 +3,8 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
+
+# Loading The Dataset
+data = pd.read_csv(r"C:\Users\sujal\OneDrive\Desktop\my_project\OIBSIP\SUJALJAISWAL_TASK02\Advertising.csv")
+
+print("++++++ SALES PREDICTION USING PYTHON ++++++\n")
