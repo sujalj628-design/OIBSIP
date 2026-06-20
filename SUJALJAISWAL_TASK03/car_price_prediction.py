2b48542359c28d2d44e50963d82fe0ba07af264d
sujalme <sujalj628@gmail.com>
2026-06-20 23:20:52 +0530

    LOADING THE DATASET AND EXPLORATION

--- SUJALJAISWAL_TASK03/car_price_prediction.py
+++ SUJALJAISWAL_TASK03/car_price_prediction.py
@@ -3,3 +3,14 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
+
+# Loading The Dataset
+data = pd.read_csv(r"C:\Users\sujal\OneDrive\Desktop\my_project\OIBSIP\SUJALJAISWAL_TASK03\car data.csv")
+
+print("++++++ CAR PRICE PREDICTION ++++++\n")
+
+# DATASET EXPLORATION
+print("Dataset Shape:", data.shape)
+
+print("\nColumns:")
+print(data.columns)
